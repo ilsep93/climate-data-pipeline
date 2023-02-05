9472aa996470640331c56e84ea298a6da2b586b9
Ilse Paniagua <ilsep@umich.edu>
2023-02-05 17:59:38 -0500

    terraform initial commit

---
+++ terraform/terraform_execution.md
@@ -0,0 +1,9 @@
+# Execution steps
+1. `terraform init`: 
+    * Initializes & configures the backend, installs plugins/providers, & checks out an existing configuration from a version control 
+2. `terraform plan`:
+    * Matches/previews local changes against a remote state, and proposes an Execution Plan.
+3. `terraform apply`: 
+    * Asks for approval to the proposed plan, and applies changes to cloud
+4. `terraform destroy`
+    * Removes your stack from the Cloud
---
+++ terraform/variables.tf
@@ -0,0 +1,24 @@
+locals {
+  data_lake_bucket = "chelsa_zonal_statistics"
+}
+
+variable "project" {
+  description = "chelsa-climate-pipeline"
+}
+
+variable "region" {
+  description = "Region for GCP resources. Choose as per your location: https://cloud.google.com/about/locations"
+  default = "us-east5"
+  type = string
+}
+
+variable "storage_class" {
+  description = "Storage class type for your bucket. Check official docs for more info."
+  default = "STANDARD"
+}
+
+variable "BQ_DATASET" {
+  description = "BigQuery Dataset that raw data (from GCS) will be written to"
+  type = string
+  default = "chelsa_datasets"
+}
