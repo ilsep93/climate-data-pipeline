2a8e6ce794258450b66cd569f7f60927edb14feb
Ilse Paniagua <ilsep@umich.edu>
2023-02-11 15:28:24 -0500

    remove download urls

--- orchestration/raster_ingestion.py
+++ orchestration/raster_ingestion.py
@@ -4,10 +4,7 @@ from zipfile import ZipFile
 import rasterio as rio
 import requests
 from prefect import flow, task
-from rasterio.plot import show
 
-#url = "https://os.zhdk.cloud.switch.ch/envicloud/chelsa/chelsa_V1/chelsa_cmip5_ts/CHELSAcmip5ts_tasmax_CMCC-CM_rcp45_2030-2049_V1.1.nc"
-url = "https://os.zhdk.cloud.switch.ch/envicloud/chelsa/chelsa_V1/cmip5/2061-2080/temp/CHELSA_tas_mon_ACCESS1-0_rcp85_r1i1p1_g025.nc_1_2061-2080_V1.2.tif"
 rast_url = "https://os.zhdk.cloud.switch.ch/envicloud/chelsa/chelsa_V1/cmip5/2061-2080/temp/CHELSA_tas_mon_ACCESS1-0_rcp85_r1i1p1_g025.nc_1_2061-2080_V1.2.tif"
 adm2_url = "https://data.humdata.org/dataset/b20cd345-93fb-43bd-9c6e-7bc7d87b63eb/resource/30b6979a-d3f3-4982-971f-dc53f076bc52/download/wca_admbnda_adm2_ocha.zip"
 
