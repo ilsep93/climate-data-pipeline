f408aa0b75b0c5cea93eb836fcf917e20f3735c7
Ilse Paniagua <ilsep@umich.edu>
2023-02-11 15:27:54 -0500

    update fetch_raster method with docstring

--- orchestration/raster_ingestion.py
+++ orchestration/raster_ingestion.py
@@ -8,19 +8,25 @@ from rasterio.plot import show
 
 #url = "https://os.zhdk.cloud.switch.ch/envicloud/chelsa/chelsa_V1/chelsa_cmip5_ts/CHELSAcmip5ts_tasmax_CMCC-CM_rcp45_2030-2049_V1.1.nc"
 url = "https://os.zhdk.cloud.switch.ch/envicloud/chelsa/chelsa_V1/cmip5/2061-2080/temp/CHELSA_tas_mon_ACCESS1-0_rcp85_r1i1p1_g025.nc_1_2061-2080_V1.2.tif"
+rast_url = "https://os.zhdk.cloud.switch.ch/envicloud/chelsa/chelsa_V1/cmip5/2061-2080/temp/CHELSA_tas_mon_ACCESS1-0_rcp85_r1i1p1_g025.nc_1_2061-2080_V1.2.tif"
 adm2_url = "https://data.humdata.org/dataset/b20cd345-93fb-43bd-9c6e-7bc7d87b63eb/resource/30b6979a-d3f3-4982-971f-dc53f076bc52/download/wca_admbnda_adm2_ocha.zip"
 
 
 @task(retries=3, log_prints=True)
-def fetch_raster(url):
-    # response = requests.get(url)
-    # print(response)
-    # print(type(response))
+def fetch_raster(url:str) -> None:
+    """Download CHELSA raster
+
+    Args:
+        url (str): URL for raster of interest
+
+    Returns:
+        _type_: rasterio object
+    """
     with rio.open(url) as rast:
+        print(type(rast))
         print(rast.count)
         print(rast.profile)
         print(rast.bounds)
-        #show(rast)
     return rast
 
 
@@ -44,7 +50,6 @@ def fetch_vector(url:str) -> None:
 
 @flow()
 def main_flow():
-    fetch_raster(url)
     fetch_raster(rast_url)
     fetch_vector(adm2_url)
 
