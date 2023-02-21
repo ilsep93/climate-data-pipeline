f1256f01c91faa45bd5eb2c22cb488b81edd331b
Ilse Paniagua <ilsep@umich.edu>
2023-02-20 21:23:26 -0500

    standardize filepath names by parametrizing in main_flow()
    Avoid setting specific paths in the function itself. Instead, specify in main_flow()

--- orchestration/raster_ingestion.py
+++ orchestration/raster_ingestion.py
@@ -1,6 +1,5 @@
 import io
 import os
-from pathlib import Path
 from zipfile import ZipFile
 
 import fiona
@@ -15,13 +14,14 @@ from rasterstats import zonal_stats
 
 rast_url = "https://os.zhdk.cloud.switch.ch/envicloud/chelsa/chelsa_V1/cmip5/2061-2080/temp/CHELSA_tas_mon_ACCESS1-0_rcp85_r1i1p1_g025.nc_1_2061-2080_V1.2.tif"
 raster_name = rast_url.split("/")[-1]
+raster_name = raster_name.replace(".tif", "")
 
 adm2_url = "https://data.humdata.org/dataset/b20cd345-93fb-43bd-9c6e-7bc7d87b63eb/resource/30b6979a-d3f3-4982-971f-dc53f076bc52/download/wca_admbnda_adm2_ocha.zip"
 shapefile_name = adm2_url.split("/")[-1]
 shapefile_name = shapefile_name.replace(".zip", "")
 
 @task(log_prints=True, retries=0)
-def write_local_raster(url:str, raster_inpath: str) -> None:
+def write_local_raster(url:str, out_path: str) -> None:
     """Download CHELSA raster and print descriptive statistics
 
     Args:
@@ -38,7 +38,7 @@ def write_local_raster(url:str, raster_inpath: str) -> None:
         raster = rast.read()
         print(type(raster))
 
-    with rasterio.open(f"{raster_inpath}/{raster_name}", "w", **profile) as dest:
+    with rasterio.open(f"{out_path}", "w", **profile) as dest:
         print(dest)
         dest.write(raster)
         
@@ -59,7 +59,7 @@ def fetch_geometry(url:str) -> ZipFile:
     return ZipFile(io.BytesIO(response.content))
     
 @task(log_prints=True)
-def extract_geometry(zip: ZipFile, adm_level:str) -> gpd.GeoDataFrame:
+def extract_geometry(zip: ZipFile, shp_path:str, adm_level: str) -> gpd.GeoDataFrame:
     """Saves shapefile locally and returns geodataframe
 
     Args:
@@ -72,29 +72,27 @@ def extract_geometry(zip: ZipFile, adm_level:str) -> gpd.GeoDataFrame:
     shapefile_name = adm2_url.split("/")[-1]
     shapefile_name = shapefile_name.replace(".zip", "")
     zip.extractall(f"data/{adm_level}")
-    return gpd.read_file(f"data/{adm_level}/{shapefile_name}.shp")
+    return gpd.read_file(f"{shp_path}")
     
 @task()
-def mask_raster(raster_inpath: str, raster_outpath: str, adm_level: str):
+def mask_raster(raw_path: str, masked_path: str, shp_path: str):
     """Mask raster with shapefile
 
     Args:
-        raster_inpath (str): Location of raw raster
-        raster_outpath (str): Save location for masked raster
+        raw_path (str): Location of raw raster
+        masked_path (str): Save location for masked raster
         adm_level (str): ADM level (eg. "adm2")
     """
     
-    with fiona.open(f"data/{adm_level}/{shapefile_name}.shp") as shapefile:
+    with fiona.open(f"{shp_path}") as shapefile:
         shapes = [feature["geometry"] for feature in shapefile]
 
-    with rasterio.open(raster_inpath, "r") as raster:
-        print(raster.crs)
+    with rasterio.open(raw_path, "r") as raster:
         profile = raster.profile
-        out_image, out_transform = rasterio.mask.mask(raster, shapes, crop=True)
+        out_image, out_transform = mask.mask(raster, shapes, crop=True)
     
-    with rasterio.open(raster_outpath, "w", **profile) as dest:
+    with rasterio.open(masked_path, "w", **profile) as dest:
         dest.write(out_image)
-        
 
 @task(log_prints=True)
 def create_zonal_statistics(masked_rast: str, zs_path: str, shp_path: str) -> None:
@@ -116,23 +114,35 @@ def create_zonal_statistics(masked_rast: str, zs_path: str, shp_path: str) -> No
 @flow(log_prints=True)
 def main_flow():
     adm_level = "adm2"
-    raster_inpath = "data/rasters/raw"
-    raster_outpath = "data/rasters/masked"
+    shp_path = f"data/{adm_level}/{shapefile_name}.shp"
+    raw_path = "data/rasters/raw"
+    masked_path = "data/rasters/masked"
+    zs_path = "data/zonal_statistics"
 
-    if os.path.exists(f"data/rasters/raw/{raster_name}") is False:
+    if os.path.exists(f"{raw_path}/{raster_name}.tif") is False:
         print("Downloading raster")
-        write_local_raster(rast_url, raster_inpath=raster_inpath)
+        write_local_raster(
+            url=rast_url,
+            out_path=f"{raw_path}/{raster_name}.tif"
+            )
 
     if os.path.exists(f"data/adm2/{shapefile_name}.shp") is False:
         print("Downloading shapefile")
         adm_zip = fetch_geometry(url=adm2_url)
-        shapefile = extract_geometry(zip=adm_zip, adm_level="adm2")
+        extract_geometry(
+            zip=adm_zip,
+            adm_level=adm_level,
+            shp_path=shp_path
+            )
     
-    if os.path.exists(f"data/rasters/masked/_masked{raster_name}") is False:
+    if os.path.exists(f"{masked_path}/masked_{raster_name}.tif") is False:
         print("Masking raster")
-        mask_raster(raster_inpath=f"{raster_inpath}/{raster_name}",
-                    raster_outpath=f"{raster_outpath}/masked_{raster_name}",
-                    adm_level = adm_level)
+        mask_raster(
+            raw_path=f"{raw_path}/{raster_name}.tif",
+            shp_path=shp_path,
+            masked_path=f"{masked_path}/masked_{raster_name}.tif"
+                    )
+
     if os.path.exists(f"{zs_path}/zs_{raster_name}") is False:
         create_zonal_statistics(
             masked_rast=f"{masked_path}/masked_{raster_name}.tif",
