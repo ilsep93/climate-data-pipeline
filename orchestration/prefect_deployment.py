aef0609d0a8377bb9cb5369140f39662eed34660
Ilse Paniagua <ilsep@umich.edu>
2023-02-23 11:15:40 -0500

    update params for prefect deployment

--- orchestration/prefect_deployment.py
+++ orchestration/prefect_deployment.py
@@ -1,14 +1,13 @@
-from prefect.blocks.core import Block
 from prefect.deployments import Deployment
 from prefect.filesystems import GitHub
 from raster_ingestion import etl_parent_flow
 
-github_block = GitHub.load("github-repo")
+github_block = GitHub.load("climate-pipeline-repo")
 
 deployment = Deployment.build_from_flow(
     flow=etl_parent_flow,
     name="github_multi_green_taxi_flow",
-    parameters={"color": "green", "year": 2020, "months": [1,2,3]},
+    parameters={"months": [1,2,3]},
     infra_overrides={"env": {"PREFECT_LOGGING_LEVEL": "DEBUG"}},
     work_queue_name="default",
     storage=github_block
