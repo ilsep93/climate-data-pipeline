03295f2c32678aadf8df793382946b23c9697057
Ilse Paniagua <ilsep@umich.edu>
2023-02-23 10:15:15 -0500

    change prefect bucket to GcsBucket

--- orchestration/make_gcp_blocks.py
+++ orchestration/make_gcp_blocks.py
@@ -1,6 +1,7 @@
-from prefect.filesystems import GCS, GitHub
+from prefect.filesystems import GitHub
 from prefect_gcp import GcpCredentials
+from prefect_gcp.cloud_storage import GcsBucket
 
 github_block = GitHub.load("climate-pipeline-repo")
 gcp_credentials_block = GcpCredentials.load("gcp-climate-pipeline")
-gcp_zs_bucket = GCS.load("gcp-zs-bucket")
+gcp_zs_bucket = GcsBucket.load("gcp-zs-bucket")
