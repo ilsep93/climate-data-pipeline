82b9927d9e49d83f0a6a3a6882dfe04760ad4dfc
Ilse Paniagua <ilsep@umich.edu>
2023-03-29 08:40:03 -0400

    update name of raster_processing script and function

--- orchestration/prefect_deployment.py
+++ orchestration/prefect_deployment.py
@@ -1,11 +1,11 @@
 from prefect.deployments import Deployment
 from prefect.filesystems import GitHub
-from raster_ingestion import etl_parent_flow
+from raster_processing import raster_processing_parent_flow
 
 github_block = GitHub.load("climate-pipeline-repo")
 
 deployment = Deployment.build_from_flow(
-    flow=etl_parent_flow,
+    flow=raster_processing_parent_flow,
     name="github_multi_green_taxi_flow",
     parameters={"months": [1,2,3]},
     infra_overrides={"env": {"PREFECT_LOGGING_LEVEL": "DEBUG"}},
