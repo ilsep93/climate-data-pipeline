2e82218576d86e605b79cc5db16780ec44ecae47
Ilse Paniagua <ilsep@umich.edu>
2023-04-19 16:18:17 -0400

    move climate urls to new file
    Avoids having urls as global variables and makes them available for use in other files

--- pipeline/raster_processing.py
+++ pipeline/raster_processing.py
@@ -6,16 +6,10 @@ import fiona
 import geopandas as gpd
 import pandas as pd
 import rasterio
+from climatology_urls import climatology_base_url
 from rasterio import mask
 from rasterstats import zonal_stats
 
-cmip5_temp = "https://os.zhdk.cloud.switch.ch/envicloud/chelsa/chelsa_V1/cmip5/2061-2080/temp"
-
-climatology_base_url = [
-    f"{cmip5_temp}/CHELSA_tas_mon_ACCESS1-0_rcp45_r1i1p1_g025.nc",
-    f"{cmip5_temp}/CHELSA_tas_mon_ACCESS1-0_rcp85_r1i1p1_g025.nc",
-    f"{cmip5_temp}/CHELSA_tas_mon_BNU-ESM_rcp26_r1i1p1_g025.nc",
-]
 
 @dataclass()
 class ClimatologyProcessing:
