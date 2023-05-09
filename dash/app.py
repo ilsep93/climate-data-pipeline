452bf4870bb8a80deb12bbccd1453c7d211bd146
Ilse Paniagua <ilsep@umich.edu>
2023-05-09 10:59:27 -0400

    update style elements of app

--- dash/app.py
+++ dash/app.py
@@ -37,75 +37,85 @@ external_stylesheets = [
 
 app = Dash(__name__, external_stylesheets=external_stylesheets)
 
-app.layout = html.Div(
 
+app.layout = html.Div(
     children=[
 
-        html.H1(children="West Africa Climate Change Analytics"),
+        html.H1(children="West Africa Climate Change Analytics", className="header-title"),
 
         html.P(
 
             children=(
-
-                "Analyze sub-national climate change projections"
-
+                "Subnational Climate Change Projections"
                 " by month between 2061-2080"
-
             ),
-
+            className="header-description"
         ),
-
-        dcc.Graph(
-
-            figure={
-
-                "data": [
-
-                    {
-
-                        "x": data["month"],
-
-                        "y": data["mean"],
-
-                        "type": "lines",
-
-                    },
-
-                ],
-
-                "layout": {"title": "Average Temperature"},
-
-            },
-
+        html.Div(
+            children=[
+                html.Div(
+                    children=dcc.Graph(
+                        id="price-chart",
+                        config={"displayModeBar": False},
+                        figure={
+                            "data": [
+                                {
+                                    "x": data["month"],
+                                    "y": data["max"],
+                                    "type": "lines",
+                                    "hovertemplate": (
+                                        "°C%{y:.2f}<extra></extra>"
+                                    ),
+                                },
+                            ],
+                            "layout": {
+                                "title": {
+                                    "text": "Maxium Projected Temperature °C",
+                                    "x": 0.05,
+                                    "xanchor": "left",
+                                },
+                                "xaxis": {"fixedrange": True},
+                                "yaxis": {
+                                    "tickprefix": "°C",
+                                    "fixedrange": True,
+                                },
+                                "colorway": ["#17b897"],
+                            },
+                        },
+                    ),
+                    className="card",
+                ),
+                html.Div(
+                    children=dcc.Graph(
+                        id="volume-chart",
+                        config={"displayModeBar": False},
+                        figure={
+                            "data": [
+                                {
+                                    "x": data["month"],
+                                    "y": data["mean"],
+                                    "type": "lines",
+                                },
+                            ],
+                            "layout": {
+                                "title": {
+                                    "text": "Average Projected Temperature °C",
+                                    "x": 0.05,
+                                    "xanchor": "left",
+                                },
+                                "xaxis": {"fixedrange": True},
+                                "yaxis": {"fixedrange": True},
+                                "colorway": ["#E12D39"],
+                            },
+                        },
+                    ),
+                    className="card",
+                ),
+            ],
+            className="wrapper",
         ),
-
-        dcc.Graph(
-
-            figure={
-
-                "data": [
-
-                    {
-
-                        "x": data["month"],
-
-                        "y": data["max"],
-
-                        "type": "lines",
-
-                    },
-
-                ],
-
-                "layout": {"title": "Max Projected Temperature"},
-
-            },
-
-        ),
-
     ]
-
 )
 
 if __name__ == "__main__":
-    app.run_server(debug=False)
+    app.run_server(debug=True)
