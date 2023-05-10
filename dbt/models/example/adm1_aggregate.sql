c53a7fac94d1da0e370f9039c94d55b04b0680f5
Ilse Paniagua <ilsep@umich.edu>
2023-05-10 10:33:39 -0400

    initial implementation of adm1 table aggregate

--- dbt/models/example/adm1_aggregate.sql
+++ dbt/models/example/adm1_aggregate.sql
@@ -1,9 +1,17 @@
-{{ config(materialized='table') }}
-
--- Convert temperature to Kelvin
-
--- Create new table with ADM0 aggregate
-
-SELECT min, max, mean, median
-FROM {{ source('your_source', 'existed_table_name') }}
-GROUP BY admin1Name
+{{ config(materialized="table") }}
+-- depends_on: {{ ref('table_1') }}
+-- depends_on: {{ ref('table_2') }}
+-- depends_on: {{ ref('table_3') }}
+{% call statement('tables_for_union', fetch_result=True) %}
+    SELECT table_name
+    FROM `dbt-test.dbt_test.INFORMATION_SCHEMA.TABLES`
+    WHERE table_name LIKE 'table_%'
+{% endcall %}
+{% set tables = load_result('tables_for_union')['data'] %}
+{% for table in tables %}
+SELECT *
+FROM {{ ref(table[0]) }}
+  {% if not loop.last %}
+    UNION ALL
+  {% endif %}
+{% endfor %}
