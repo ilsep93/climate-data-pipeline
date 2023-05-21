8b36c63d871569fd61dba8bce6c8518deee260a0
Ilse Paniagua <ilsep@umich.edu>
2023-05-21 19:17:53 -0400

    create nested list of adm0 options
    rename to adm1 options list

--- dash/nested_adms.py
+++ dash/nested_adms.py
@@ -9,8 +9,13 @@ import pandas as pd
 # For now, using local dataset to create options list
 df = pd.read_csv("data/ACCESS1-0_rcp45/time_series/ACCESS1-0_rcp45_yearly.csv")
 data = df[['admin0Name', 'admin1Name', 'admin2Name']]
+
+adm0_list = data.admin0Name.unique()
 adm1_list = data.admin1Name.unique()
 
+adm1_options_dict = dict()
+for adm0 in adm0_list:
+    adm1_options_dict[adm0] = list(data[['admin1Name']][data.admin0Name == adm0]
                               .squeeze()
                               .unique())
 
