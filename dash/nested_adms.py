e399f8130f5fac3d61a21a6186e67ed20be1142f
Ilse Paniagua <ilsep@umich.edu>
2023-05-21 19:04:42 -0400

    add example structure from documentation

---
+++ dash/nested_adms.py
@@ -0,0 +1,5 @@
+# Example structure from documentation: https://dash.plotly.com/basic-callbacks
+# all_options = {
+#     'America': ['New York City', 'San Francisco', 'Cincinnati'],
+#     'Canada': [u'Montréal', 'Toronto', 'Ottawa']
+# }
