72b6d65d7d5b6905c511d2fef4f8a945218280cc
Ilse Paniagua <ilsep@umich.edu>
2023-05-22 15:14:08 -0400

    delete example from documentation

--- dash/nested_adms.py
+++ dash/nested_adms.py
@@ -1,10 +1,5 @@
 import pandas as pd
 
-# Example structure from documentation: https://dash.plotly.com/basic-callbacks
-# all_options = {
-#     'America': ['New York City', 'San Francisco', 'Cincinnati'],
-#     'Canada': [u'Montréal', 'Toronto', 'Ottawa']
-# }
 
 # For now, using local dataset to create options list
 df = pd.read_csv("data/ACCESS1-0_rcp45/time_series/ACCESS1-0_rcp45_yearly.csv", encoding='utf-8-sig')
