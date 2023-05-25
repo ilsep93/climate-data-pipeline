5b2e9ebebabc6ef722f8ebe5523455791ea06293
Ilse Paniagua <ilsep@umich.edu>
2023-05-25 16:14:40 -0400

    initial attempt at decoding french characters

--- utils/read_db_table.py
+++ utils/read_db_table.py
@@ -10,5 +10,8 @@ def get_climatology_table():
             table_name = 'union_table'
             # Fetch the table data using a select query
             query = text(f"SELECT * FROM {schema_name}.{table_name}")
-            df = pd.read_sql(query, session.get_bind().connect())
+            df = pd.read_sql(query, session.get_bind().connect(), )
+            # columns_to_encode = ["admin0name", "admin1name", "admin2name"]
+            # for column in df[columns_to_encode]:
+            #     df[column] = df[column].str.decode("utf-8-sig")
     return df
