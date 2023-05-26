cf2b9903b87f0dcbe5d423e15bc10dc98099071a
Ilse Paniagua <ilsep@umich.edu>
2023-05-26 14:13:38 -0400

    initial implementation of dowload geojson
    Format of json is still tricky to work with, but adding to tackle in another feature

--- pipeline/vector_processing.py
+++ pipeline/vector_processing.py
@@ -1,4 +1,5 @@
 import io
+import json
 import logging
 import os
 from pathlib import Path
@@ -10,6 +11,7 @@ import requests
 logger = logging.getLogger(__name__)
 logging.basicConfig(filename='adm.log', encoding='utf-8', level=logging.DEBUG)
 
+ROOT_DIR = Path(__file__).parent.parent
 
 def download_shapefile(
     out_path: Path,
@@ -34,12 +36,25 @@ def download_shapefile(
         response.raise_for_status()
         data = ZipFile(io.BytesIO(response.content))
         data.extractall(f"{out_path}")
+
         logger.info(f"Downloaded shapefile: {shapefile_location}")
     else:
         logger.info("Shapefile exists in provided location")
 
 
 def download_geojson(
+    url: str,
+    outpath: Path):
+
+    response = requests.get(url, allow_redirects=True)
+    response.raise_for_status()
+    content = response.content
+    json_data = json.loads(content)
+    # JSON data to geojson
+    # geojson = gpd.read_file(json_data.decode(), driver = 'GeoJSON')
+    # geojson.to_file(f"{outpath}.geojson", driver='GeoJSON')
+    return json_data
+
 def shapefile_to_geojson(
         shp_path: Path,
         outpath: Path
@@ -66,3 +81,6 @@ if __name__ == "__main__":
         shp_path=Path(f"{ROOT_DIR}/data/adm2/"),
         outpath=Path("{ROOT_DIR}/dash/west_africa")
         )
+    # download_geojson(
+    #     url="https://data.humdata.org/dataset/b20cd345-93fb-43bd-9c6e-7bc7d87b63eb/resource/6166e76c-bc33-4c45-8031-649d76aa5644/download/wca_admbnda_adm2_ocha.json",
+    #     outpath="../dash/west_africa.geojson")
