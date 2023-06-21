0f5d1eeda67d8f3b88073e8da2f17a08b59ed9ea
Ilse Paniagua <ilsep@umich.edu>
2023-06-21 14:45:26 -0400

    enum of scenarios for download
    Can be extended, many more scenarios are available for download

--- pipeline/climatology.py
+++ pipeline/climatology.py
@@ -13,6 +13,15 @@ class Climatology(Enum):
 
     def _url_to_climatology(self, climatology_url):
         self.climatology = re.search(string=climatology_url, pattern="CHELSA_tas_mon_(.*_rcp\d\d)").group(1)
+
+class Scenario(Enum):
+    ACCESS1_0_rcp45 = "ACCESS1-0_rcp45"
+    ACCESS1_0_rcp85 = "ACCESS1-0_rcp85"
+    BNU_ESM_rcp26 = "BNU-ESM_rcp26"
+    CCSM4_rcp60 = "CCSM4_rcp60"
+    BNU_ESM_rcp45 = "BNU-ESM_rcp45"
+
+
     
     def _climatology_pathways(self, climatology_url):   
         self._url_to_climatology(climatology_url)
