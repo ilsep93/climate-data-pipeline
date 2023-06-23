d3a60d6d468ef98ef6531adf7073bcf3c5f7c48f
Ilse Paniagua <ilsep@umich.edu>
2023-06-23 14:34:27 -0400

    remove unused files
    No longer using Prefect blocks. TODO: implement airflow and add files to S3 (rather than local)

--- pipeline/make_gcp_blocks.py
+++
@@ -1,7 +0,0 @@
-from prefect.filesystems import GitHub
-from prefect_gcp import GcpCredentials
-from prefect_gcp.cloud_storage import GcsBucket
-
-github_block = GitHub.load("climate-pipeline-repo")
-gcp_credentials_block = GcpCredentials.load("gcp-climate-pipeline")
-gcp_zs_bucket = GcsBucket.load("gcp-zs-bucket")
