99871576d97fb2d223f147a3fe744f3dcd95c431
Ilse Paniagua <ilsep@umich.edu>
2023-06-24 15:27:23 -0400

    change name of test fixtures to sample_object

--- tests/test_climatology_processing.py
+++ tests/test_climatology_processing.py
@@ -9,6 +9,25 @@ from pipeline.climatology_processing import (read_raster_from_url,
                                              write_local_raster)
 
 
+@pytest.fixture(scope="session")
+def climatology_url(random_climatology, random_month, random_scenario):
+    url = random_climatology.get_url(scenario=random_scenario, month=random_month)
+    return url
+
+@pytest.fixture(scope="session")
+def sample_raster(climatology_url):
+    raster, _ = read_raster(location=climatology_url)
+    return raster
+
+@pytest.fixture(scope="session")
+def sample_profile(climatology_url):
+    _ , profile = read_raster(location=climatology_url)
+    return profile
+
+@pytest.fixture(scope="session")
+def sample_shapefile():
+    ...
+
 class TestClimatologyProcessing:
     def test_read_raster(self, climatology_url):
         raster, profile = read_raster(location=climatology_url)
