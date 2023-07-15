68aa325b9d056079e4090c468a137d4bd0d1d916
Ilse Paniagua <ilsep@umich.edu>
2023-07-15 12:24:07 -0400

    cleanup unused imports and __main__ example

--- pipeline/climatology_uploads.py
+++ pipeline/climatology_uploads.py
@@ -1,15 +1,10 @@
 import logging
-import sys
-from dataclasses import dataclass
 from datetime import datetime
 from pathlib import Path
 
 import pandas as pd
-from climatology import Month, Scenario
 from dotenv import load_dotenv
-from sqlalchemy.types import DateTime, Float, String
 
-#sys.path.append("db/")
 from db.base_table import BaseTable
 from db.session import get_session
 
@@ -25,8 +20,8 @@ logger = logging.getLogger(__name__)
 
 def upload_to_db(df_path: Path, table_name: str, schema: str) -> None:
 
+    df = pd.read_csv(df_path, encoding= 'unicode_escape')
 
-            logger.info(f"Uploaded '{self.climatology}' to the DB")
     with get_session() as Session:
         with Session() as session:
             try:
@@ -42,3 +37,8 @@ def upload_to_db(df_path: Path, table_name: str, schema: str) -> None:
 
 
 if __name__ == "__main__":
+    upload_to_db(
+        df_path=Path("data/cmip5/temp/ACCESS1-0_rcp45/time_series/ACCESS1-0_rcp45_yearly.csv"),
+        table_name="test_table",
+        schema="test_schema"
+    )
