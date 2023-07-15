d3d37ef9338a4ca95b562e78244a82bc432b233d
Ilse Paniagua <ilsep@umich.edu>
2023-07-15 12:14:37 -0400

    convert from class to function

--- pipeline/climatology_uploads.py
+++ pipeline/climatology_uploads.py
@@ -18,50 +18,13 @@ load_dotenv("docker/.env")
 logger = logging.getLogger(__name__)
 logging.basicConfig(filename='uploads_logger.log', encoding='utf-8', level=logging.DEBUG)
 
-@dataclass
-class ClimatologyUploads(Climatology):
-    climatology_url: str
-    schema: str = "climatology"
-    
-    def upload_to_db(
-        self
-        ) -> None:
-        """Uploads yearly aggregated table to postgres db
-        Processing steps:
-        * Reduce number of columns to adm names and projected values
-        * Convert int to datetime
-        * Add climatology column based on climatology name
 
-        Args:
-            engine (Engine): Engine connection to postgres db
-        """
-        self._climatology_pathways(self.climatology_url)
-        df = pd.read_csv(f"{self.time_series}/{self.climatology}_yearly.csv", encoding= 'unicode_escape')
+load_dotenv("docker/.env")
 
-        df.columns= df.columns.str.lower() # lowercase columns
-        keep_cols = ['admin0name', 'admin1name', 'admin2name', 'min', 'max', 'mean', 'median', 'month']
-        df = df[keep_cols] #subset to set of cols
+logger = logging.getLogger(__name__)
 
-        df["month"] = df["month"].apply(lambda x: datetime.strptime(str(x), "%m")) #transform from int to date
-        df["climatology"] = self.climatology
+def upload_to_db(df_path: Path, table_name: str, schema: str) -> None:
 
-        with get_session() as Session:
-            with Session() as session:
-                df.to_sql(name=self.climatology.lower(),
-                      schema=self.schema,
-                      con=session.get_bind(),
-                      if_exists='replace',
-                      index=False,
-                      dtype={
-                        "admin0name": String,
-                        "admin1name": String,
-                        "admin2name": String,
-                        "min": Float,
-                        "max": Float,
-                        "mean": Float,
-                        "median": Float,
-                        "month": DateTime
-                        })
 
             logger.info(f"Uploaded '{self.climatology}' to the DB")
 
