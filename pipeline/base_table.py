6d14c3dfc369a097e2207589f2feca2d19f19241
Ilse Paniagua <ilsep@umich.edu>
2023-08-10 16:18:49 -0400

    turn to mixin that inherits from object class
    Mixins do not inherit from SQLAlchemy declarative bases

--- pipeline/base_table.py
+++ pipeline/base_table.py
@@ -1,12 +1,11 @@
 from sqlalchemy import Column, DateTime, Float, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import declared_attr, relationship
+from sqlalchemy.orm import declared_attr
 
-Base = declarative_base()
 
-class TableMixin(Base):
+class BaseTable:
     id = Column(Integer, primary_key=True)
     iso3_code = relationship('Country')
+    iso2_code = Column(String(2))
     adm0_name = Column(String(128))
     adm1_name = Column(String(128))
     adm2_name = Column(String(128))
@@ -19,11 +18,3 @@ class TableMixin(Base):
     min_raw = Column(Float)
     max_raw = Column(Float)
     uploaded_at = Column(DateTime, nullable=False)
-
-    @declared_attr
-    def __tablename__(cls):
-        return cls.__name__.lower()
-    
-    @declared_attr
-    def __table_args__(cls):
-        return cls.metadata.schema
