58009ece3cdefb07fe4b75c1f1af99a9e88f946a
Ilse Paniagua <ilsep@umich.edu>
2023-08-10 16:14:57 -0400

    black formatting

--- pipeline/climatology.py
+++ pipeline/climatology.py
@@ -6,6 +6,7 @@ from pathlib import Path
 
 class Product(Enum):
     """CHELSA products available under CMIP"""
+
     TEMP = "temp"
     BIO = "bio"
     PREC = "prec"
@@ -17,6 +18,7 @@ class Scenario(Enum):
     """Sample of scenarios available under CMIP.
     List can be expanded by users
     """
+
     ACCESS1_0_rcp45 = "ACCESS1-0_rcp45"
     ACCESS1_0_rcp85 = "ACCESS1-0_rcp85"
     BNU_ESM_rcp26 = "BNU-ESM_rcp26"
@@ -26,12 +28,14 @@ class Scenario(Enum):
 
 class Phase(Enum):
     """CMIP phases available as of 2023"""
+
     CMIP5 = "cmip5"
     CMIP6 = "cmip6"
 
 
 class Month(Enum):
     """Months of the year available as CHELSA simulations"""
+
     JANUARY = 1
     FEBRUARY = 2
     MARCH = 3
@@ -45,6 +49,7 @@ class Month(Enum):
     NOVEMBER = 11
     DECEMBER = 12
 
+
 class ChelsaProduct(ABC):
     """Abstract class for all CHELSA Product products"""
 
@@ -55,7 +60,6 @@ class ChelsaProduct(ABC):
     scenarios: list[Scenario]
     months: list[Month]
 
-
     def get_url(self, scenario: Scenario, month: Month) -> str:
         """Constructs a URL based on a given scenario and month for a given product.
 
@@ -63,17 +67,20 @@ class ChelsaProduct(ABC):
             download_url: URL that can be used to download raster .tif file
         """
         if scenario not in self.scenarios:
-            raise ValueError(f"This product is not available. \
-                         Options include {self.scenarios}")
-        
+            raise ValueError(
+                f"This product is not available. \
+                         Options include {self.scenarios}"
+            )
+
         if month not in self.months:
-            raise ValueError(f"This month is not available. \
-                         Options include {self.months}")
+            raise ValueError(
+                f"This month is not available. \
+                         Options include {self.months}"
+            )
 
         download_url = f"https://os.zhdk.cloud.switch.ch/envicloud/chelsa/chelsa_V1/{self.phase.value}/{self.time_period}/{self.product.value}/CHELSA_{self.base_url}_mon_{scenario.value}_r1i1p1_g025.nc_{month.value}_{self.time_period}_V1.2.tif"
         return download_url
 
-    
     def set_pathways_as_attributes(self, scenario: Scenario, month: Month):
         """Generate directories and file paths as class attributes
         Used to determine if files are already available, or should
@@ -85,43 +92,59 @@ class ChelsaProduct(ABC):
         """
 
         from config import read_config
+
         config = read_config("config.json")
 
-        base_path = Path(f"{config.root_dir}/{self.phase.value}/{self.product.value}/{scenario.value}/")
+        base_path = Path(
+            f"{config.root_dir}/{self.phase.value}/{self.product.value}/{scenario.value}/"
+        )
 
         self.raw_raster_dir = Path(f"{base_path}/{config.raw_raster_dir}/")
         self.cropped_raster_dir = Path(f"{base_path}/{config.cropped_raster_dir}/")
         self.zonal_stats_dir = Path(f"{base_path}/{config.zonal_stats_dir}/")
         self.yearly_aggregate_dir = Path(f"{base_path}/{config.yearly_aggregate_dir}/")
-        
-        self.raw_raster_path = Path(f"{self.raw_raster_dir}/{scenario.value}_{month.value}.tif")
-        self.cropped_raster_path = Path(f"{self.cropped_raster_dir}/{scenario.value}_{month.value}.tif")
-        self.zonal_file_path = Path(f"{self.zonal_stats_dir}/{scenario.value}_{month.value}.csv")
-        self.yearly_aggregate_path = Path(f"{self.yearly_aggregate_dir}/{scenario.value}_{month.value}_yearly.csv")
 
-        directories = [self.raw_raster_dir, self.cropped_raster_dir, self.zonal_stats_dir, self.yearly_aggregate_dir]
+        self.raw_raster_path = Path(
+            f"{self.raw_raster_dir}/{scenario.value}_{month.value}.tif"
+        )
+        self.cropped_raster_path = Path(
+            f"{self.cropped_raster_dir}/{scenario.value}_{month.value}.tif"
+        )
+        self.zonal_file_path = Path(
+            f"{self.zonal_stats_dir}/{scenario.value}_{month.value}.csv"
+        )
+        self.yearly_aggregate_path = Path(
+            f"{self.yearly_aggregate_dir}/{scenario.value}_{month.value}_yearly.csv"
+        )
+
+        directories = [
+            self.raw_raster_dir,
+            self.cropped_raster_dir,
+            self.zonal_stats_dir,
+            self.yearly_aggregate_dir,
+        ]
 
         self._create_directories(pathways=directories)
 
     def _create_directories(self, pathways: list) -> None:
         """Create local directories to save downloaded and processed data"""
 
-
         for path in pathways:
             if not os.path.exists(path):
                 os.makedirs(path, exist_ok=True)
 
-    
+
 class Temperature(ChelsaProduct):
     """Concrete implementation of temperature ChelsaProduct"""
 
     product = Product.TEMP
-    scenarios = [Scenario.ACCESS1_0_rcp45,
-                 Scenario.ACCESS1_0_rcp85,
-                 Scenario.BNU_ESM_rcp26,
-                 Scenario.BNU_ESM_rcp45,
-                 Scenario.CCSM4_rcp60,
-                 ]
+    scenarios = [
+        Scenario.ACCESS1_0_rcp45,
+        Scenario.ACCESS1_0_rcp85,
+        Scenario.BNU_ESM_rcp26,
+        Scenario.BNU_ESM_rcp45,
+        Scenario.CCSM4_rcp60,
+    ]
     months = [month for month in Month]
     base_url = "tas"
 
@@ -130,12 +153,13 @@ class Bio(ChelsaProduct):
     """Concrete implementation of bio ChelsaProduct"""
 
     product = Product.BIO
-    scenarios = [Scenario.ACCESS1_0_rcp45,
-                 Scenario.ACCESS1_0_rcp85,
-                 Scenario.BNU_ESM_rcp26,
-                 Scenario.BNU_ESM_rcp45,
-                 Scenario.CCSM4_rcp60,
-                 ]
+    scenarios = [
+        Scenario.ACCESS1_0_rcp45,
+        Scenario.ACCESS1_0_rcp85,
+        Scenario.BNU_ESM_rcp26,
+        Scenario.BNU_ESM_rcp45,
+        Scenario.CCSM4_rcp60,
+    ]
     months = [month for month in Month]
     base_url = "bio"
 
@@ -144,46 +168,50 @@ class Precipitation(ChelsaProduct):
     """Concrete implementation of precipitation ChelsaProduct"""
 
     product = Product.PREC
-    scenarios = [Scenario.ACCESS1_0_rcp45,
-                 Scenario.ACCESS1_0_rcp85,
-                 Scenario.BNU_ESM_rcp26,
-                 Scenario.BNU_ESM_rcp45,
-                 Scenario.CCSM4_rcp60,
-                 ]
+    scenarios = [
+        Scenario.ACCESS1_0_rcp45,
+        Scenario.ACCESS1_0_rcp85,
+        Scenario.BNU_ESM_rcp26,
+        Scenario.BNU_ESM_rcp45,
+        Scenario.CCSM4_rcp60,
+    ]
     months = [month for month in Month]
     base_url = "pr"
 
 
 class MaximumTemperature(ChelsaProduct):
     """Concrete implementation of maxiumum temperature ChelsaProduct"""
-    
+
     product = Product.TMAX
-    scenarios = [Scenario.ACCESS1_0_rcp45,
-                 Scenario.ACCESS1_0_rcp85,
-                 Scenario.BNU_ESM_rcp26,
-                 Scenario.BNU_ESM_rcp45,
-                 Scenario.CCSM4_rcp60,
-                 ]
+    scenarios = [
+        Scenario.ACCESS1_0_rcp45,
+        Scenario.ACCESS1_0_rcp85,
+        Scenario.BNU_ESM_rcp26,
+        Scenario.BNU_ESM_rcp45,
+        Scenario.CCSM4_rcp60,
+    ]
     months = [month for month in Month]
     base_url = "tasmax"
 
 
 class MinimumTemperature(ChelsaProduct):
     """Concrete implementation of minimum temperature ChelsaProduct"""
-    
+
     product = Product.TMIN
-    scenarios = [Scenario.ACCESS1_0_rcp45,
-                 Scenario.ACCESS1_0_rcp85,
-                 Scenario.BNU_ESM_rcp26,
-                 Scenario.BNU_ESM_rcp45,
-                 Scenario.CCSM4_rcp60,
-                 ]
+    scenarios = [
+        Scenario.ACCESS1_0_rcp45,
+        Scenario.ACCESS1_0_rcp85,
+        Scenario.BNU_ESM_rcp26,
+        Scenario.BNU_ESM_rcp45,
+        Scenario.CCSM4_rcp60,
+    ]
     months = [month for month in Month]
     base_url = "tasmin"
 
 
 class TemperatureProduct(Enum):
     """Convenience class to identify temperature products"""
+
     Temperature = auto()
     MinimumTemperature = auto()
     MaximumTemperature = auto()
@@ -201,8 +229,10 @@ def get_climatology(product: str) -> ChelsaProduct:
     available_products = [product.value for product in Product]
     lower_case_product = str(product.value).lower()
     if lower_case_product not in available_products:
-        raise ValueError(f"This product is not available. \
-                         Options include {available_products}")
+        raise ValueError(
+            f"This product is not available. \
+                         Options include {available_products}"
+        )
 
     factories = {
         "temp": Temperature(),
--- pipeline/processing_steps.py
+++ pipeline/processing_steps.py
@@ -2,7 +2,8 @@ import os
 from enum import Enum, auto
 from pathlib import Path
 
-from climatology import ChelsaProduct, Month, Scenario
+from climatology import ChelsaProduct, Month, Product, Scenario
+from climatology_uploads import _check_if_table_exists, upload_to_db
 from config import read_config
 from crop import process_masked_raster
 from download import process_raw_raster
@@ -13,41 +14,48 @@ from zonal_stats import process_zonal_statistics
 logger = setup_logger()
 
 config = read_config("config.json")
+
+
 class RasterProcessingStep(Enum):
     DOWNLOAD = auto()
     MASK = auto()
     ZONAL_STATISTICS = auto()
     YEARLY_TABLE = auto()
+    UPLOAD = auto()
 
 
-def get_processing_steps(product:
-                         ChelsaProduct,
-                         scenario: Scenario,
-                         month: Month
-                         ) -> list[RasterProcessingStep]:
+def get_processing_steps(
+    product: ChelsaProduct, scenario: Scenario, month: Month
+) -> list[RasterProcessingStep]:
     """Determine which processing steps are needed for a given month of the product's scenario.
     Each pipeline run is for a specific product, month, and scenario pair.
-    
+
     TODO: Log pipeline runs on postgres database.
     """
 
     product.set_pathways_as_attributes(scenario=scenario, month=month)
 
     processing_steps = []
-    
+
     if not os.path.exists(product.raw_raster_path):
         processing_steps.append(RasterProcessingStep.DOWNLOAD)
 
     if not os.path.exists(product.cropped_raster_path):
         processing_steps.append(RasterProcessingStep.MASK)
-    
+
     if not os.path.exists(product.zonal_file_path):
         processing_steps.append(RasterProcessingStep.ZONAL_STATISTICS)
 
-    all_months_available = _check_monthly_zonal_stats_complete(zonal_path=Path(product.zonal_stats_dir))
+    all_months_available = _check_monthly_zonal_stats_complete(
+        zonal_path=Path(product.zonal_stats_dir)
+    )
     if all_months_available and not os.path.exists(product.yearly_aggregate_path):
         processing_steps.append(RasterProcessingStep.YEARLY_TABLE)
-    
+
+    # table_exists = _check_if_table_exists(table_name=f"{scenario}_{month}")
+    # if not table_exists:
+    processing_steps.append(RasterProcessingStep.UPLOAD)
+
     return processing_steps
 
 
@@ -67,10 +75,12 @@ def _check_monthly_zonal_stats_complete(zonal_path: Path) -> bool:
         return False
 
 
-def execute_processing_steps(processing_steps: list[RasterProcessingStep],
-                             chelsa_product: ChelsaProduct,
-                             scenario: Scenario,
-                             month: Month) -> None:
+def execute_processing_steps(
+    processing_steps: list[RasterProcessingStep],
+    chelsa_product: ChelsaProduct,
+    scenario: Scenario,
+    month: Month,
+) -> None:
     """Execute downloads, cropping, zonal statistics, and yearly table depending on processing steps
 
     Args:
@@ -79,31 +89,45 @@ def execute_processing_steps(processing_steps: list[RasterProcessingStep],
         scenario (Scenario): Scenario to be processed
         month (Month): Month to be processed
     """
-
     if RasterProcessingStep.DOWNLOAD in processing_steps:
-        process_raw_raster(product=chelsa_product,
-                        scenario=scenario,
-                        month=month,
-                        raw_out_path=chelsa_product.raw_raster_path)
-        
+        process_raw_raster(
+            product=chelsa_product,
+            scenario=scenario,
+            month=month,
+            raw_out_path=chelsa_product.raw_raster_path,
+        )
+
     if RasterProcessingStep.MASK in processing_steps:
+        process_masked_raster(
+            raw_raster_location=chelsa_product.raw_raster_path,
+            masked_out_path=chelsa_product.cropped_raster_path,
+        )
 
-        process_masked_raster(raw_raster_location=chelsa_product.raw_raster_path,
-                            masked_out_path=chelsa_product.cropped_raster_path)
-    
     if RasterProcessingStep.ZONAL_STATISTICS in processing_steps:
-        process_zonal_statistics(raster_location=chelsa_product.cropped_raster_path,
-                                out_path=chelsa_product.zonal_file_path,
-                                product=chelsa_product,
-                                scenario=scenario,
-                                month=month,
-                                place_id=config.adm_unique_id)
-    
-    if RasterProcessingStep.YEARLY_TABLE in processing_steps:
-        process_yearly_table(product = chelsa_product,
-                            zonal_dir=chelsa_product.zonal_stats_dir,
-                            out_path=chelsa_product.yearly_aggregate_path,
-                            sort_values=[config.adm_unique_id, "month"])
-    
-    if len(processing_steps) == 0:
+        process_zonal_statistics(
+            raster_location=chelsa_product.cropped_raster_path,
+            out_path=chelsa_product.zonal_file_path,
+            product=chelsa_product,
+            scenario=scenario,
+            month=month,
+            place_id=config.adm_unique_id,
+        )
 
+    if RasterProcessingStep.YEARLY_TABLE in processing_steps:
+        process_yearly_table(
+            product=chelsa_product,
+            zonal_dir=chelsa_product.zonal_stats_dir,
+            out_path=chelsa_product.yearly_aggregate_path,
+            sort_values=[config.adm_unique_id, "month"],
+        )
+
+    if RasterProcessingStep.UPLOAD in processing_steps:
+        upload_to_db(
+            df_path=chelsa_product.zonal_file_path,
+            table_name=chelsa_product.product.value,
+        )
+
+    if len(processing_steps) == 0:
+        logger.info(
+            f"All available steps already completed for {chelsa_product}_{scenario.name}_{month.name}"
+        )
