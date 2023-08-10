1dc5c5e6e1931e14bace162799666cb1c53209ca
Ilse Paniagua <ilsep@umich.edu>
2023-08-10 16:16:37 -0400

    remove schema arg from get_session

--- pipeline/session.py
+++ pipeline/session.py
@@ -8,16 +8,15 @@ from sqlalchemy.orm import sessionmaker
 load_dotenv("docker/.env")
 
 @contextmanager
-def get_session(schema: str):
+def get_session():
     """Yield database session"""
     username=os.getenv("POSTGRES_USER")
     password=os.getenv("POSTGRES_PASSWORD")
     host="localhost"
     db=os.getenv("POSTGRES_DB")
     port=os.getenv("LOCAL_PORT")
-    schema_name=schema
 
-    engine = create_engine(f'postgresql://{username}:{password}@{host}:{port}/{db}?search_path={schema_name}')
+    engine = create_engine(f'postgresql://{username}:{password}@{host}:{port}/{db}')
     session_maker = sessionmaker(bind=engine)
 
     yield session_maker
