b1c6283290f20a5030e1ce397847d202aa103df1
Ilse Paniagua <ilsep@umich.edu>
2023-08-10 16:29:04 -0400

    black formatting

--- pipeline/main.py
+++ pipeline/main.py
@@ -24,30 +24,37 @@ def run_single_month(product: str, scenario: Scenario, month: Month):
     chelsa_product = get_climatology(product=product)
     chelsa_product.set_pathways_as_attributes(scenario=scenario, month=month)
 
-    # Determine which processing steps are needed for product's scenario 
-    processing_steps = get_processing_steps(product=chelsa_product,
-                                            scenario=scenario,
-                                            month=month)
+    # Determine which processing steps are needed for product's scenario
+    processing_steps = get_processing_steps(
+        product=chelsa_product, scenario=scenario, month=month
+    )
     processing_step_names = [step.name for step in processing_steps]
-    
-    logger.info(f"Processing steps: {processing_step_names} for {chelsa_product.product.name}_{scenario.name}_{month.name}")
 
-    execute_processing_steps(processing_steps=processing_steps,
-                             chelsa_product=chelsa_product,
-                             scenario=scenario,
-                             month=month)
+    logger.info(
+        f"Processing steps: {processing_step_names} for {chelsa_product.product.name}_{scenario.name}_{month.name}"
+    )
+
+    execute_processing_steps(
+        processing_steps=processing_steps,
+        chelsa_product=chelsa_product,
+        scenario=scenario,
+        month=month,
+    )
 
 
 def run_all_months(product: str, scenario: Scenario):
     """All months for a given product's scenario"""
     available_months = [month for month in Month]
-    
+
     for month in available_months:
         run_single_month(product=product, scenario=scenario, month=month)
 
     logging.shutdown()
 
-if __name__=="__main__":
-    raster_processing_flow(product=Product[config.product],
-                                  scenario=Scenario[config.scenario],
-                                  month=Month[config.month])
+
+if __name__ == "__main__":
+    raster_processing_flow(
+        product=Product[config.product],
+        scenario=Scenario[config.scenario],
+        month=Month[config.month],
+    )
