cefd95e3727c9dd8fe277db9abf203b02e6d1145
Ilse Paniagua <ilsep@umich.edu>
2023-08-18 10:23:11 -0400

    black formatting

--- pipeline/zonal_stats.py
+++ pipeline/zonal_stats.py
@@ -7,13 +7,14 @@ from vector_processing import COLUMN_MAPPING, get_geometry
 
 config = read_config("config.json")
 
+
 def process_zonal_statistics(
-        raster_location: Path,
-        out_path: Path,
-        place_id: str,
-        geom_path: Path = config.geom_path,
-       
-        ) -> None:
+    raster_location: Path,
+    out_path: Path,
+    chelsa_product: ChelsaProduct,
+    place_id: str,
+    geom_path: Path = config.geom_path,
+) -> None:
     """Processes zonal statistics for a CHELSA product
 
     Args:
@@ -23,14 +24,12 @@ def process_zonal_statistics(
         place_id (str): Column that contains a unique ID per geometry
         geom_path (Path, optional): Path to geometry used for zonal statistics. Defaults to config.geom_path.
     """
+    geometry = get_geometry(geom_path=geom_path, column_mapping=COLUMN_MAPPING)
+    zonal_stats = calculate_zonal_statistics(
+        raster_location=raster_location,
+        geometry=geometry,
+        chelsa_product=chelsa_product,
+        place_id=place_id,
+    )
 
-    geometry = get_geometry(geom_path=geom_path,
-                            column_mapping=COLUMN_MAPPING)
-    zonal_stats = calculate_zonal_statistics(raster_location=raster_location,
-                                             geometry=geometry,
-                                             product=product,
-                                             scenario=scenario,
-                                             month=month,
-                                             place_id=place_id)
-    
-    zonal_stats.to_csv(out_path, encoding='utf-8', index=False)
+    zonal_stats.to_csv(out_path, encoding="utf-8", index=False)
